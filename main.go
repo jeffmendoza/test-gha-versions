6f395ec51276d35b8383427b18684a2dff5c6d32
Jeff Mendoza <jlm@jlm.name>
2024-08-22 09:54:19 -0700

    Ko settings
    Signed-off-by: Jeff Mendoza <jlm@jlm.name>

--- main.go
+++ main.go
@@ -8,11 +8,23 @@ import (
 var Ref string
 var RefName string
 var RefType string
+var Branch string
+var Tag string
+var Commit string
+var Date string
+var TS string
+var State string
 
 func main() {
 	fmt.Printf("Ref: %q\n", Ref)
 	fmt.Printf("RefName: %q\n", RefName)
 	fmt.Printf("RefType: %q\n", RefType)
+	fmt.Printf("Branch: %q\n", Branch)
+	fmt.Printf("Tag: %q\n", Tag)
+	fmt.Printf("Commit: %q\n", Commit)
+	fmt.Printf("Date: %q\n", Date)
+	fmt.Printf("TS: %q\n", TS)
+	fmt.Printf("State: %q\n", State)
 	if b, ok := debug.ReadBuildInfo(); ok {
 		for _, s := range b.Settings {
 			if s.Key == "vcs.revision" {
